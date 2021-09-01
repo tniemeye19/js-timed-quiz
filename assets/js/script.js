16d7594fb5d67d8266a231bae23e2724bcfa5a28
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-09-01 16:37:46 -0400

    added to index and style to create the questions, completion, and high score page. added to js to create question objects

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+        number: 1,
+        question: "Commonly used data types DO NOT include:",
+        answer: "3. alerts",
+        selections: [
+            "1. strings",
+            "2. booleans",
+            "3. alerts",
+            "4. numbers"
+        ]
+    },
+    {
+        number: 2,
+        question: "The condition if an if / else statement is enclosed with ______.",
+        answer: "3. parenthesis",
+        selections: [
+            "1. quotes",
+            "2. curly brackets",
+            "3. parenthesis",
+            "4. square brackets"
+        ]
+    },
+    {
+        number: 3,
+        question: "Arrays in JavaScript can be used to store ______.",
+        answer: "4. all of the above",
+        selections: [
+            "1. numbers and strings",
+            "2. other arrays",
+            "3. booleans",
+            "4. all of the above"
+        ]
+    },
+    {
+        number: 4,
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        answer: "3. quotes",
+        selections: [
+            "1. commas",
+            "2. curly brackets",
+            "3. quotes",
+            "4. parenthesis"
+        ]
+    },
+    {
+        number: 5,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: "4. console.log",
+        selections: [
+            "1. JavaScript",
+            "2. terminal/bash",
+            "3. for loops",
+            "4. console.log"
+        ]
+    }
+];
+
+var startBtn = document.querySelector('.start-quiz .start-btn');
