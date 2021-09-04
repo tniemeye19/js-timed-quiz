60f9f3f19ace46def7060d1ab0c1eb65601e3305
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-09-04 02:40:14 -0400

    evaluate and increment function added in order to cycle through next question function. still in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,13 +51,9 @@ var completeEl = document.querySelector("#complete-screen");
 var highscoreEl = document.querySelector("#highscore-screen");
 var questionTitleEl = document.querySelector("#question-title");
 var selectionsEl = document.querySelector(".selection");
-var choiceOneClickEl = document.querySelector("#s1").addEventListener("click", nextQuestion);
 var choiceOneEl = document.querySelector("#s1");
-var choiceTwoClickEl = document.querySelector("#s2").addEventListener("click", nextQuestion);
 var choiceTwoEl = document.querySelector("#s2");
-var choiceThreeClickEl = document.querySelector("#s3").addEventListener("click", nextQuestion);
 var choiceThreeEl = document.querySelector("#s3");
-var choiceFourClickEl = document.querySelector("#s4").addEventListener("click", nextQuestion);
 var choiceFourEl = document.querySelector("#s4");
 var wrongORrightEl = document.querySelector("#wrongORright");
 var userFinalScoreEl = document.querySelector("#final-score");
@@ -65,92 +61,6 @@ var userFinalScoreEl = document.querySelector("#final-score");
 // Initialize variables for use within functions
 score = 0;
 
-
-// var qOne = function () {
-//     questionTitleEl.textContent = questionsObj[0].q;
-//     choiceOneEl.textContent = questionsObj[0].c1;
-//     choiceTwoEl.textContent = questionsObj[0].c2;
-//     choiceThreeEl.textContent = questionsObj[0].c3;
-//     choiceFourEl.textContent = questionsObj[0].c4;
-//     if (choiceOneEl.clicked === true ||  
-//         choiceTwoEl.clicked === true || 
-//         choiceFourEl.clicked === true) {
-//         wrongORrightEl.textContent = "Wrong!";
-//         count += 1;
-//         timeRemaining -= 10;
-//     } else if (choiceThreeEl.clicked === true) {
-//         wrongORrightEl.textContent = "Correct!";
-//         count += 1;
-//     }
-// };
-
-// var qTwo = function () {
-//     questionTitleEl.textContent = questionsObj[1].q;
-//     choiceOneEl.textContent = questionsObj[1].c1;
-//     choiceTwoEl.textContent = questionsObj[1].c2;
-//     choiceThreeEl.textContent = questionsObj[1].c3;
-//     choiceFourEl.textContent = questionsObj[1].c4;
-//     if (choiceOneEl.clicked === true ||  
-//         choiceTwoEl.clicked === true || 
-//         choiceFourEl.clicked === true) {
-//         wrongORrightEl.textContent = "Wrong!";
-//         count += 1;
-//     } else if (choiceThreeEl.clicked === true) {
-//         wrongORrightEl.textContent = "Correct!";
-//         count += 1;
-//     }
-// };
-
-// var qThree = function () {
-//     questionTitleEl.textContent = questionsObj[2].q;
-//     choiceOneEl.textContent = questionsObj[2].c1;
-//     choiceTwoEl.textContent = questionsObj[2].c2;
-//     choiceThreeEl.textContent = questionsObj[2].c3;
-//     choiceFourEl.textContent = questionsObj[2].c4;
-//     if (choiceOneEl.clicked === true ||  
-//         choiceTwoEl.clicked === true || 
-//         choiceThreeEl.clicked === true) {
-//         wrongORrightEl.textContent = "Wrong!";
-//         count += 1;
-//     } else if (choiceFourEl.clicked === true) {
-//         wrongORrightEl.textContent = "Correct!";
-//         count += 1;
-//     }
-// };
-
-// var qFour = function () {
-//     questionTitleEl.textContent = questionsObj[3].q;
-//     choiceOneEl.textContent = questionsObj[3].c1;
-//     choiceTwoEl.textContent = questionsObj[3].c2;
-//     choiceThreeEl.textContent = questionsObj[3].c3;
-//     choiceFourEl.textContent = questionsObj[3].c4;
-//     if (choiceOneEl.clicked === true ||  
-//         choiceTwoEl.clicked === true || 
-//         choiceFourEl.clicked === true) {
-//         wrongORrightEl.textContent = "Wrong!";
-//         count += 1;
-//     } else if (choiceThreeEl.clicked === true) {
-//         wrongORrightEl.textContent = "Correct!";
-//         count += 1;
-//     }
-// };
-
-// var qFive = function () {
-//     questionTitleEl.textContent = questionsObj[4].q;
-//     choiceOneEl.textContent = questionsObj[4].c1;
-//     choiceTwoEl.textContent = questionsObj[4].c2;
-//     choiceThreeEl.textContent = questionsObj[4].c3;
-//     choiceFourEl.textContent = questionsObj[4].c4;
-//     if (choiceOneEl.clicked === true ||  
-//         choiceTwoEl.clicked === true || 
-//         choiceThreeEl.clicked === true) {
-//         wrongORrightEl.textContent = "Wrong!";
-//         count += 1;
-//     } else if (choiceFourEl.clicked === true) {
-//         wrongORrightEl.textContent = "Correct!";
-//         count += 1;
-//     };
-// };
 
 // Initialize Variables for Timer
 timerEl.textContent = 0;
@@ -174,44 +84,47 @@ startBtn.addEventListener('click', function() {
 });
 
 
-
+currentQuestionIndex = 0;
 var nextQuestion = function() {
     console.log("nextQuestion function executed");
-    qObj = questionsObj.length;
-    for (var i = 0; i < qObj; i++) {
-        questionTitleEl.textContent = questionsObj[i].q;
-        choiceOneEl.textContent = questionsObj[i].c1;
-        choiceTwoEl.textContent = questionsObj[i].c2;
-        choiceThreeEl.textContent = questionsObj[i].c3;
-        choiceFourEl.textContent = questionsObj[i].c4;
+
+    function evaluateAndIncrement() {
+        if ((currentQuestion.c1 === currentQuestion.a) ||
+            (currentQuestion.c1 === currentQuestion.a) ||
+            (currentQuestion.c1 === currentQuestion.a) ||
+            (currentQuestion.c1 === currentQuestion.a)) {
+            console.log("You are correct!");
+            wrongORrightEl.textContent = "Correct!";
+            score = score + 20;
+            console.log(score);
+            currentQuestionIndex = currentQuestionIndex + 1;
+            nextQuestion();
+
+        } else {
+            console.log("You are wrong!")
+            wrongORrightEl.textContent = "Wrong!";
+            timeRemaining = timeRemaining - 10;
+            currentQuestionIndex = currentQuestionIndex + 1;
+            nextQuestion();
+
+        }
     }
-    // for (var i = 0; i <= 5; i++) {
-    //     if (questionsObj[i] === 1) {
-    // };
 
-    // var ii = questionsObj.length;
+    var currentQuestionIndex = 0;
+    var currentQuestion = questionsObj[currentQuestionIndex];
+    qObj = questionsObj.length;
+    for (var currentQuestionIndex = 0; currentQuestionIndex < qObj; currentQuestionIndex++) {
 
-    // for (i = 0; i < ii; i++) {
-    //     switch(questionsObj[i]) {
-    //         case 0:
-    //             questionTitleEl.textContent = questionsObj[i].q;
-    //             choiceOneEl.textContent = questionsObj[i].c1;
-    //             choiceTwoEl.textContent = questionsObj[i].c2;
-    //             choiceThreeEl.textContent = questionsObj[i].c3;
-    //             choiceFourEl.textContent = questionsObj[i].c4;
-    //             break;
-    //     }
-    
-    
-    // i = 0;
-    // for (i = 0; i <= 4; ++i) {
-    //     questionTitleEl.textContent = questionsObj[0].q;
-    //     choiceOneEl.textContent = questionsObj[0].c1;
-    //     choiceTwoEl.textContent = questionsObj[0].c2;
-    //     choiceThreeEl.textContent = questionsObj[0].c3;
-    //     choiceFourEl.textContent = questionsObj[0].c4;
-    //     choiceOneClickEl;
-    //     console.log(i);
-    // }
-    // }
+        questionTitleEl.textContent = currentQuestion.q;
+        choiceOneEl.textContent = currentQuestion.c1;
+        choiceTwoEl.textContent = currentQuestion.c2;
+        choiceThreeEl.textContent = currentQuestion.c3;
+        choiceFourEl.textContent = currentQuestion.c4;
+        var choiceOneClickEl = document.querySelector("#s1").addEventListener("click", evaluateAndIncrement);
+        var choiceTwoClickEl = document.querySelector("#s2").addEventListener("click", evaluateAndIncrement);
+        var choiceThreeClickEl = document.querySelector("#s3").addEventListener("click", evaluateAndIncrement);
+        var choiceFourClickEl = document.querySelector("#s4").addEventListener("click", evaluateAndIncrement);
+        console.log(currentQuestionIndex);
+    }
+
 };
